f1416c36944d023c92b0142aac3134a07f7061bd
Ben Skeen <ben@redpeppersoftware.com>
2018-03-19 22:36:18 -0600

    I initialized the server and the front end.

---
+++ SecurityTestApp.Api/Startup.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace SecurityTestApp.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            //app.Use(async (context, next) =>
+            //{
+            //    if (context.Request.Path.HasValue && !context.Request.Path.Value.StartsWith("/api", StringComparison.OrdinalIgnoreCase))
+            //    {
+            //        context.Request.Path = new PathString("/");
+            //    }
+
+            //    await next();
+            //});
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
---
+++ SecurityTestApp.Business/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SecurityTestApp.Business
+{
+    public class Class1
+    {
+    }
+}
